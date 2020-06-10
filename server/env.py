7b5d4cbf166dd66f2bfd4d5768c7bf8b708879f9
DimkaPusha@gmail.com <Dimkach3>
2020-06-10 16:04:36 +0300

    push commit

---
+++ server/env.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ['DB_HOST'] = '35.198.160.119'
+os.environ['DB_FULL_NAME'] = 'webgene:europe-west3:users-data-db2'
+os.environ['DB_DATABASE'] = 'users-data'
+os.environ['DB_USERNAME'] = 'postgres'
+os.environ['DB_PASSWORD'] = '12345678'
+os.environ['DB_PORT'] = '5432'
+os.environ['BUCKET_NAME'] = 'backend-basket'
